87d3f3a57f6c94732fe88c9811889cff7ae84d11
Changran <xuchangran111@163.com>
2023-03-22 13:24:50 +0800

    Add_GoBack
    -Add goback in popup menu

--- src/PopupMenu.js
+++ src/PopupMenu.js
@@ -1,10 +1,11 @@
 import React, { Component, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 import './PopupMenu.css';
 
 export default function PopupMenu(){
-    
+    const navigate = useNavigate();
     
     return(
         <Popup trigger={<button className="home-page-btn">Trigger</button>} modal nested>
@@ -20,10 +21,10 @@ export default function PopupMenu(){
             </div>
             <div className="quit">
                 <button className="quit-btn">Quit</button>
-
             </div>
- 
- 
+            <div className="goback">
+                <button className="goback-btn" onClick={() => { navigate(-1);}}>Go Back</button>
+            </div>
         </Popup>
 
 
