5aa097a4a7d2ede4119fbf1b8e6a8f3b9c241859
develop555ben <develop555ben@gmail.com>
2023-04-11 23:30:38 +0800

    Setting page UI

--- Unity_File/PacMan3D/Assets/Script/NetworkManager.cs
+++ Unity_File/PacMan3D/Assets/Script/NetworkManager.cs
@@ -4,9 +4,39 @@ using UnityEngine;
 
 public class NetworkManager : Manager<NetworkManager>
 {
+
+    private const string SERVERIP = "";
+
     public static void SaveMapToServer(MapJson mapJson)
     {
         var jsonStr = mapJson.jsonStr();
         // TODO
     }
+
+    public static MapJson LoadMapFromServer()
+    {
+        //TODO
+        string jsonStr = "";
+        MapJson mapJson = JsonUtility.FromJson<MapJson>(jsonStr);
+        return mapJson;
+    }
+
+    public static User GetUserInfoFromServer()
+    {
+        //TODO
+        string jsonStr = "";
+        User user = JsonUtility.FromJson<User>(jsonStr);
+        return user;
+    }
+
+    public static void GetSettingFromServer()
+    {
+        //TODO
+    }
+
+
+
+    
+
+
 }
