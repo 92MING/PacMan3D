76eea12a8b113c399dc3965d81cdafc9198e3fa5
92MING <yashin.sd123@gmail.com>
2023-04-12 16:07:58 +0800

    add dropdown

--- Unity_File/PacMan3D/Assets/Script/UI/SettingPage.cs
+++ Unity_File/PacMan3D/Assets/Script/UI/SettingPage.cs
@@ -1,12 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+public class LanguageDropDown : TranslateEnumDropdown<Language> { }
 
 public class SettingPage : UIPage
 {
+    [SerializeField] private Slider _bgmSlider;
+    [SerializeField] private Slider _sfxSlider;
+
+    private LanguageDropDown _languageDropDown;
+    [SerializeField] private RectTransform _langDropdown_template;
+    [SerializeField] private Text _langDropdown_captionText;
+    [SerializeField] private Text _langDropdown_itemText;
+    [SerializeField] private GameObject _langDropdown_object;
+
     private void Awake()
     {
         base.Awake();
         UIManager.settingPage = this;
+        if (_langDropdown_object.TryGetComponent<Dropdown>(out var dropdown))
+        {
+            DestroyImmediate(dropdown);
+        }
+        _languageDropDown = _langDropdown_object.AddComponent<LanguageDropDown>();
+        _languageDropDown.template = _langDropdown_template;
+        _languageDropDown.captionText = _langDropdown_captionText;
+        _languageDropDown.itemText = _langDropdown_itemText;
     }
 }
---
+++ Unity_File/PacMan3D/Assets/Script/UI/TranslateEnumDropdown.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Scripting;
+using UnityEngine.UI;
+
+public class TranslateEnumDropdown<Enum> : Dropdown where Enum : System.Enum
+{
+    readonly System.Array _values = System.Enum.GetValues(typeof(Enum));
+    readonly string[] _keys = System.Enum.GetNames(typeof(Enum));
+    
+    public Enum currentChoice => (Enum)_values.GetValue(value);
+    public string currentChoiceKey => _keys[value];
+    public string currentChoiceText => options[value].text;
+
+    void Awake()
+    {
+        base.Awake();
+        SystemManager.OnLanguageChanged.AddListener((lang) => { updateOptionTexts(); });
+        options.Clear();
+        for (int i = 0; i < _values.Length; i++)
+        {
+            var opt = new Dropdown.OptionData();
+            SystemManager.TryGetTranslation(_keys[i], out var translatedText);
+            opt.text = translatedText;
+            options.Add(opt);
+        }
+    }
+
+    void updateOptionTexts()
+    {
+        for (int i=0; i<_values.Length; i++)
+        {
+            SystemManager.TryGetTranslation(_keys[i], out var translatedText);
+            options[i].text = translatedText;
+        }
+    }
+}
